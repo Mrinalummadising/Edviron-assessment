1472b2dfd6d466aa7c1c6114fcaee56b3687ea2d
mrinalummadising <138893163+Mrinalummadising@users.noreply.github.com>
2025-01-13 11:55:29 +0530

    edviron dashboard

---
+++ server/database/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://test-user:edviron@edvironassessment.ub8p5.mongodb.net/?retryWrites=true&w=majority&appName=edvironAssessment"
+    );
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = dbConnection;
